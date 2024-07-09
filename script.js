df1b3bd6dbcaa5aebcb2d36699b62a3095ef4db9
sd535682 <sd535682@gmail.com>
2024-07-10 04:01:08 +0530

    beta

---
+++ script.js
@@ -0,0 +1,7 @@
+function closeSidebar() {
+  document.querySelector(".sidebar").style.display = "none";
+}
+
+function openSidebar() {
+  document.querySelector(".sidebar").style.display = "block";
+}
